defb10a96ab2d70e7dacec1c1b3519c93bdc6ecf
Alex <alex.d.nax@gmail.com>
2025-10-27 17:05:48 +0100

    fix build

--- packages/levva-agent/sql/secrets.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO public.levva_secrets VALUES ('6aa0d72e-c6f8-42b8-8d45-dad39736ee57', '0x2d1af72c0978fcc3cea0a6b6d9784fd1e84048484db9e21d8e48682c08c2a79b', 'api_key', 'Default key');
-INSERT INTO public.levva_secrets VALUES ('fbce7174-5e21-4fab-84cf-ba3d05e37f88', '0xab8b4edc7abdc222b6d43abb7f3bffca081fcdf73de62b40f6ce437b11378415', 'api_key', 'Warden key');
